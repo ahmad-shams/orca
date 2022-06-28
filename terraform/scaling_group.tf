6c7cf2eb7e8feda90b23163fef86ee6cd23c4b77
ashams <ahmad.f.shams@gmail.com>
2022-06-28 22:43:53 +0300

    set docker pass as env variable

--- terraform/scaling_group.tf
+++ terraform/scaling_group.tf
@@ -15,32 +15,32 @@ resource "aws_placement_group" "this" {
 }
 
 
-# resource "aws_autoscaling_group" "mygroup" {
-#   name                      = "auto-scaling-orca-web"
-#   max_size                  = 3
-#   min_size                  = 1
-#   health_check_grace_period = 30
-#   health_check_type         = "EC2"
-#   desired_capacity          = 2
-#   force_delete         = true
-#   launch_configuration = aws_launch_configuration.as_conf.name
-#   vpc_zone_identifier  = module.vpc.private_subnets
-#   target_group_arns    = module.alb.target_group_arns
-#   # availability_zones   = module.vpc.azs
-#   depends_on = [
-#     aws_launch_configuration.as_conf
-#   ]
-# }
+resource "aws_autoscaling_group" "mygroup" {
+  name                      = "auto-scaling-orca-web"
+  max_size                  = 3
+  min_size                  = 1
+  health_check_grace_period = 30
+  health_check_type         = "EC2"
+  desired_capacity          = 2
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.as_conf.name
+  vpc_zone_identifier  = module.vpc.private_subnets
+  target_group_arns    = module.alb.target_group_arns
+  # availability_zones   = module.vpc.azs
+  depends_on = [
+    aws_launch_configuration.as_conf
+  ]
+}
 
 
-# # Creating the autoscaling policy of the autoscaling group
-# resource "aws_autoscaling_policy" "mygroup_policy" {
-#   name                   = "autoscalegroup_policy"
-#   scaling_adjustment     = 2
-#   adjustment_type        = "ChangeInCapacity"
-#   cooldown               = 300
-#   autoscaling_group_name = aws_autoscaling_group.mygroup.name
-# }
+# Creating the autoscaling policy of the autoscaling group
+resource "aws_autoscaling_policy" "mygroup_policy" {
+  name                   = "autoscalegroup_policy"
+  scaling_adjustment     = 2
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.mygroup.name
+}
 
 resource "aws_iam_instance_profile" "asg" {
   name = "asg"
