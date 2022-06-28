6f98bdebc45d70c2dae0ba2c301632f79cb8fcad
ashams <ahmad.f.shams@gmail.com>
2022-06-28 22:27:58 +0300

    set docker pass as env variable

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id  DATABASE_URL --query SecretString --output text)
-docker pull 565105851053.dkr.ecr.us-east-1.amazonaws.com/orca_python_docker:latest
+{
+    DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id  DATABASE_URL --query SecretString --output text)
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 565105851053.dkr.ecr.us-east-1.amazonaws.com/orca_python_docker
 
-docker run  565105851053.dkr.ecr.us-east-1.amazonaws.com/orca_python_docker:latest
+    docker pull 565105851053.dkr.ecr.us-east-1.amazonaws.com/orca_python_docker:latest
+
+    docker run  --env DATABASE_URL=$DATABASE_URL 565105851053.dkr.ecr.us-east-1.amazonaws.com/orca_python_docker:latest
+
+} | tee /tmp/run-docker.log
